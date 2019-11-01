d6876d0ea5e177fb7f72b83c6df9d37f37042e26
Hsinhua <yha0971@gmail.com>
2019-11-01 18:25:38 +0800

    init

---
+++ webp.js
@@ -0,0 +1,16 @@
+const imagemin = require('imagemin');
+const webp = require('imagemin-webp');
+
+const convertImages = async () => {
+    const files = ['./input/*.{jpg,png}'];
+    const config = {
+        destination: './output',
+        plugins: [webp({ quality: 75 })],
+    };
+
+    console.log('開始轉換圖片⋯⋯');
+    await imagemin(files, config);
+    console.log('已將圖片轉成 WebP！');
+};
+
+convertImages();
